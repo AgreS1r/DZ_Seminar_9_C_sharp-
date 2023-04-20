b9088085552b42f4300d240a2eb8d9c10d7f4da9
danil <faceitplay2004@mail.ru>
2023-04-20 20:53:41 +0300

    exercises 64 66 68

---
+++ 66z/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+SumFromStartToEnd(M, N, 0);
+
+void SumFromStartToEnd (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    SumFromStartToEnd(numberM, numberN, sum);
+}
+
---
+++ 68z/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Даны два неотрицательных числа M и N. Напишите программу вычисления функции Аккермана с помощью рекурсии.
+
+Console.Clear();
+Console.Write("Введите неотричательное число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите неотрицательное число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана для чисел ({M},{N}) = {AckermanFunction(M, N)}");
+
+int AckermanFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermanFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermanFunction(numberM - 1, AckermanFunction(numberM, numberN - 1));
+return AckermanFunction(numberM, numberN);
+}
+
